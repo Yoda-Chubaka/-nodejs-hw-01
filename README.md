af98602bfec3869ce6daca1dcaf2273290aed1e8
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-10-26 15:15:44 +0300

    New commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+https://monosnap.com/file/a6PG9loYzr5t5VfmpBCak8zK09gObb
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+https://monosnap.com/file/F9A93LqzqMfLNm1Uu40INAzRiV5iSL
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+https://monosnap.com/file/1HSvOs9JO8T6bnrHqYEUvIsrmBR7sc
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+https://monosnap.com/file/FBm3ybl5GdMKfcxmj2UvATxB8p8D1Z
---
+++ contacts.js
@@ -0,0 +1,52 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+}
+
+async function writeContacts(contacts) {
+  return await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(contact => contact.id === contactId);
+    return result || null;
+}
+
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await writeContacts(contacts);
+    return result;
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone,
+    };
+    contacts.push(newContact);
+    await writeContacts(contacts);
+    return newContact;
+}
+
+module.exports = {
+    listContacts,
+    writeContacts,
+    getContactById,
+    removeContact,
+    addContact
+}
